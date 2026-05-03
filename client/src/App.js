f30848594a4af8327d453ef99f6272e4552d617d
Vishal A S <vishal872002@gmail.com>
2026-05-04 01:08:33 +0530

    FIX: Production client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { ApolloProvider } from "@apollo/client/react";
+import { ApolloProvider } from "@apollo/client";
 import client from "./services/apolloClient";
 import { JobProvider } from "./context/JobContext";
 import BoardPage from "./pages/BoardPage";
