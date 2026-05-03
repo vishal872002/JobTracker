02097701bedda4ab3538d6b8b7d282fb19f036b1
Vishal A S <vishal872002@gmail.com>
2026-05-03 17:06:45 +0530

    Deploy: Commit made from VS Code Azure Static Web Apps

--- client/src/services/apolloClient.js
+++ client/src/services/apolloClient.js
@@ -1,11 +1,9 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-
-const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URL || "http://localhost:5001/graphql";
-
-const httpLink = createHttpLink({ uri: GRAPHQL_URI, credentials: "same-origin" });
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: httpLink,
+  uri: import.meta.env.PROD
+    ? "https://jobapptracker-api.azurewebsites.net/graphql"
+    : "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
 
--- server/index.js
+++ server/index.js
@@ -1,34 +1,52 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@as-integrations/express4";
-import { typeDefs } from "./graphql/typeDefs.js";
-import { resolvers } from "./graphql/resolvers.js";
-import connectDB from "./db/connect.js";
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import { ApolloServer } from "@apollo/server";
+// import { expressMiddleware } from "@as-integrations/express4";
+// import { typeDefs } from "./graphql/typeDefs.js";
+// import { resolvers } from "./graphql/resolvers.js";
+// import connectDB from "./db/connect.js";
 
-const app = express();
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
-// app.use(express.json());
-// app.use(cors({ origin: true, credentials: true }));
+// const app = express();
+// const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
+// // app.use(express.json());
+// // app.use(cors({ origin: true, credentials: true }));
 
-await connectDB();
-// const server = new ApolloServer({ typeDefs, resolvers });
-const server = new ApolloServer({
-   typeDefs,
-   resolvers,
-   introspection: true,
- });
-await server.start();
+// await connectDB();
+// // const server = new ApolloServer({ typeDefs, resolvers });
+// const server = new ApolloServer({
+//    typeDefs,
+//    resolvers,
+//    introspection: true,
+//  });
+// await server.start();
 
-app.use(
-  '/graphql',
-  cors(),
-  express.json(),
-  expressMiddleware(server),
-);
+// app.use(
+//   '/graphql',
+//   cors(),
+//   express.json(),
+//   expressMiddleware(server),
+// );
 
-const PORT = Number(process.env.PORT || 5001);
-app.listen(PORT, () => {
-  console.log(`GraphQL server ready at http://localhost${PORT}/graphql`);
-});
+// const PORT = Number(process.env.PORT || 5001);
+// app.listen(PORT, () => {
+//   console.log(`GraphQL server ready at http://localhost${PORT}/graphql`);
+// });
+
+require("dotenv").config();
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { typeDefs } = require("./graphql/typeDefs");
+const { resolvers } = require("./graphql/resolvers");
+const connectDB = require("./db/connect");
+
+async function main() {
+  await connectDB();
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT || 5000 },
+  });
+  console.log(`🚀 GraphQL server ready at ${url}`);
+}
+
+main();
