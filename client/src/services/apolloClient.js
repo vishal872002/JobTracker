ced4085eca4880ae1ceb9d8b1d0d2e8dfdd5b77b
Vishal A S <vishal872002@gmail.com>
2026-05-04 09:55:48 +0530

    FIX: Client production link

--- client/src/services/apolloClient.js
+++ client/src/services/apolloClient.js
@@ -1,9 +1,13 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: process.env.NODE_ENV === "production"
     ? "https://jobapptracker-api.azurewebsites.net/graphql"
     : "http://localhost:5000/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
